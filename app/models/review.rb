198fa12c746d871b6ec21027b43513ae16653117
Sara Schultz <saschultz95@gmail.com>
2017-06-30 13:32:19 -0700

    pass spec for validating presence of rating of review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-  validates(:author, :content_body, :presence => true)
+  validates(:author, :content_body, :rating, :presence => true)
 
   belongs_to :product
 end
