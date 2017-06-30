44bc7205846f99a9b1b978e72376a710db2b3bbf
Sara Schultz <saschultz95@gmail.com>
2017-06-30 15:23:20 -0700

    add functionality to update a coffee

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,20 @@ class ProductsController < ApplicationController
       redirect_to products_path
     else
       render :new
-    end    
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
 private
