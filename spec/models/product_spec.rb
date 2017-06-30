5351a65303d5bf55035203a90e138b557fa7a9ca
Sara Schultz <saschultz95@gmail.com>
2017-06-30 13:28:35 -0700

    pass spec for relating reviews to a product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,5 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :origin }
   it { should validate_presence_of :notes }
+  it { should have_many :reviews}
 end
