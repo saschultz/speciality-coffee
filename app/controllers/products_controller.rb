b28072649a0b4e7961f17a8706f8074cae134cd8
Sara Schultz <saschultz95@gmail.com>
2017-06-30 15:07:53 -0700

    add partials for form and errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,23 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end    
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :origin, :notes)
+  end
+
 end
