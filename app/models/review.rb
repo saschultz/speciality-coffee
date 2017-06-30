c148ae09ad457c5c8ccd6429f13153e1a0a694f5
Sara Schultz <saschultz95@gmail.com>
2017-06-30 13:30:33 -0700

    pass spec for validating author of review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
+  validates(:author, :presence => true)
+
   belongs_to :product
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe Review do
+  it { should validate_presence_of :author }
   it { should belong_to :product }
 end
