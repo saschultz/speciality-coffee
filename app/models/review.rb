3ac7e52c86c6f6eff64da115ed55c6a5a46f580d
Sara Schultz <saschultz95@gmail.com>
2017-07-03 13:02:22 -0700

    update length of review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,11 @@
 class Review < ActiveRecord::Base
   validates(:author, :content_body, :rating, :presence => true)
-  validates_length_of(:content_body, :minimum => 50, :maximum => 250)
 
   belongs_to :product
+
+  before_save(:validate_length)
+
+  def validate_length
+    validates_length_of(:content_body, :minimum => 50, :maximum => 250)
+  end
 end
