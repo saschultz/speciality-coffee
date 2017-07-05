e0f31234df0e1c4212323f0eda2ebbc1f6a4d8d1
Sara Schultz <saschultz95@gmail.com>
2017-07-04 21:08:44 -0700

    correct jquery undefined bug

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,8 @@
 class Review < ActiveRecord::Base
-  validates(:author, :rating, :presence => true)
+  validates(:author, :presence => true)
   validates(:content_body, {length: { in: 50..250 }, :presence => true})
+  validates(:rating, {inclusion: { in: 1..5 }, :presence => true})
+
   belongs_to :product
 
-  # before_save(:validate_length)
-  #
-  # def validate_length
-  #   validates_length_of(:content_body, :minimum => 50, :maximum => 250)
-  # end
 end
