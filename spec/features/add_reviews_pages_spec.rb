2196df2b3ea4e97925ff875aa5e97794ab807c5e
Sara Schultz <saschultz95@gmail.com>
2017-07-04 22:37:34 -0700

    pass integration spec for errors when field of a review is left blank

--- spec/features/add_reviews_pages_spec.rb
+++ spec/features/add_reviews_pages_spec.rb
@@ -11,4 +11,15 @@ describe 'the adding a review process' do
     click_on 'Create Review'
     expect(page).to have_content 'Sway'
   end
+
+  it 'gives an error if a field is left blank' do
+    coffee = Product.create(:name => 'Cool Covfefe', :cost => 14.95, :origin => 'Kirundo, Burundi', :notes => 'citrus, bright, fruity')
+    visit product_path(coffee)
+    click_link 'add review'
+    fill_in 'Author', :with => ''
+    fill_in 'Content body', :with => 'This was the coolest covfefe I have ever tweeted about.'
+    fill_in 'Rating', :with => 5
+    click_on 'Create Review'
+    expect(page).to have_content 'errors'
+  end
 end
