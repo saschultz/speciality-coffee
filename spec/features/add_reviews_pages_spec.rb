9d7ec3d1cb0e29e2257270ea42b40240a36f4e4b
Sara Schultz <saschultz95@gmail.com>
2017-07-04 22:38:56 -0700

    pass integration spec for errors when a rating of a review is outside of value scope

--- spec/features/add_reviews_pages_spec.rb
+++ spec/features/add_reviews_pages_spec.rb
@@ -22,4 +22,15 @@ describe 'the adding a review process' do
     click_on 'Create Review'
     expect(page).to have_content 'errors'
   end
+
+  it 'gives an error if the rating field is greater than 5' do
+    coffee = Product.create(:name => 'Cool Covfefe', :cost => 14.95, :origin => 'Kirundo, Burundi', :notes => 'citrus, bright, fruity')
+    visit product_path(coffee)
+    click_link 'add review'
+    fill_in 'Author', :with => ''
+    fill_in 'Content body', :with => 'This was the coolest covfefe I have ever tweeted about.'
+    fill_in 'Rating', :with => 6
+    click_on 'Create Review'
+    expect(page).to have_content 'errors'
+  end
 end
