aeceaebb9222bd9ccc54dcad4d245f5dabf08247
Sara Schultz <saschultz95@gmail.com>
2017-07-09 12:36:55 -0700

    add flash message for successfully updating a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
+      flash[:notice] = "Coffee successfully updated!"
       redirect_to products_path
     else
       render :edit
